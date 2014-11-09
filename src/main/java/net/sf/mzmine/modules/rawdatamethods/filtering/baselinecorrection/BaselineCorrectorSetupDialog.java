28e6e6005a25ee5a790466271c8c1cafeb28f1f0
Golgauth Gauthier <gauthier.boaglio@gmail.com>
2014-11-09 20:13:02 +0000

    Threading enabled (the preview can now be canceled by pressing ESCape key + implemented a progressbar to show the processing completion level). Particularly useful when using a small value for "m/z bins width" parameter, as well as abusive parameters depending on the chosen baseline correction method. Using threads also avoids freezing the preview window while processing the data to be plotted.
    Former-commit-id: 9ecae24d4a611bdc58f96372b3a65a797e0141d8

--- src/main/java/net/sf/mzmine/modules/rawdatamethods/filtering/baselinecorrection/BaselineCorrectorSetupDialog.java
+++ src/main/java/net/sf/mzmine/modules/rawdatamethods/filtering/baselinecorrection/BaselineCorrectorSetupDialog.java
@@ -19,7 +19,19 @@
 
 package net.sf.mzmine.modules.rawdatamethods.filtering.baselinecorrection;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.swing.BorderFactory;
+import javax.swing.JProgressBar;
+import javax.swing.border.Border;
 
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
@@ -43,10 +55,65 @@ import net.sf.mzmine.util.Range;
  * @author Gauthier Boaglio
  * @date Nov 6, 2014
  */
-public class BaselineCorrectorSetupDialog extends ParameterSetupDialogWithChromatogramPreview {
+public class BaselineCorrectorSetupDialog extends ParameterSetupDialogWithChromatogramPreview  {
+
+	// Logger.
+	private static final Logger LOG = Logger.getLogger(BaselineCorrectionTask.class.getName());
 
 	private ParameterSet correctorParameters;
 	private BaselineCorrector baselineCorrector;
+
+	private PreviewThread previewThread = null;
+	private JProgressBar progressBar;
+	private ProgressThread progressThread = null;
+
+	// Listen to VK_ESCAPE KeyEvent and allow to abort preview computing 
+	// if input parameters gave a computation task that is about to take forever...
+	KeyListener keyListener = new KeyListener() {
+		@Override
+		public void keyPressed(KeyEvent ke) {
+			int keyCode = ke.getKeyCode();
+			if (keyCode == KeyEvent.VK_ESCAPE) {
+				if (previewThread != null) { 
+					previewThread.kill(); 
+					previewThread = null; 
+				}
+				progressBar.setVisible(false);
+				hidePreview();
+			}
+		}
+		@Override
+		public void keyReleased(KeyEvent ke) {  }
+		@Override
+		public void keyTyped(KeyEvent ke) {  }
+	};
+
+	public static List<Component> getAllComponents(final Container c) {
+		Component[] comps = c.getComponents();
+		List<Component> compList = new ArrayList<Component>();
+		for (Component comp : comps) {
+			compList.add(comp);
+			if (comp instanceof Container) {
+				compList.addAll(getAllComponents((Container) comp));
+			}
+		}
+		return compList;
+	}
+	
+	private void set_VK_ESCAPE_KeyListener() {
+		// Set VK_ESCAPE KeyEvent listeners
+		List<Component> comps = BaselineCorrectorSetupDialog.getAllComponents(BaselineCorrectorSetupDialog.this);
+		for (Component c : comps) {
+			c.addKeyListener(BaselineCorrectorSetupDialog.this.keyListener);
+		}
+	}
+	private void unset_VK_ESCAPE_KeyListener() {
+		// Remove VK_ESCAPE KeyEvent listeners
+		List<Component> comps = BaselineCorrectorSetupDialog.getAllComponents(BaselineCorrectorSetupDialog.this);
+		for (Component c : comps) {
+			c.removeKeyListener(BaselineCorrectorSetupDialog.this.keyListener);
+		}
+	}
 
 	/**
 	 * 
@@ -69,6 +136,149 @@ public class BaselineCorrectorSetupDialog extends ParameterSetupDialogWithChroma
 		// Default plot type. Initialized according to the chosen chromatogram type.
 		this.setPlotType( (this.baselineCorrector.getChromatogramType() == ChromatogramType.TIC) ? 
 				PlotType.TIC : PlotType.BASEPEAK );
+
+
+	}
+
+
+	class PreviewThread extends Thread {
+
+		private TICPlot ticPlot;
+		private RawDataFile dataFile;
+		private	Range rtRange;
+		private Range mzRange;
+		private boolean crashed;
+
+		public PreviewThread(TICPlot ticPlot, RawDataFile dataFile, Range rtRange, Range mzRange) {
+			this.ticPlot = ticPlot;
+			this.dataFile = dataFile;
+			this.rtRange = rtRange;
+			this.mzRange = mzRange;
+			this.crashed = false;
+		}
+
+		@Override
+		public void run() {
+
+			// Set VK_ESCAPE KeyEvent listeners
+			set_VK_ESCAPE_KeyListener();
+
+			ticPlot.removeAllTICDataSets();
+			ticPlot.setPlotType(getPlotType());
+
+			// Add the original raw data file
+			int scanNumbers[] = dataFile.getScanNumbers(1, rtRange);
+			TICDataSet ticDataset = new TICDataSet(dataFile, scanNumbers, mzRange, null, getPlotType());
+			ticPlot.addTICDataset(ticDataset);
+
+			this.crashed = true;
+			try {
+				// Create a new corrected raw data file
+				RawDataFile newDataFile = baselineCorrector.correctDatafile(dataFile, correctorParameters);
+
+				// If successful, add the new data file
+				if (newDataFile != null) {
+					int newScanNumbers[] = newDataFile.getScanNumbers(1, rtRange);
+					TICDataSet newDataset = new TICDataSet(newDataFile, newScanNumbers, mzRange, null, getPlotType());
+					ticPlot.addTICDataset(newDataset);
+
+					// Show the trend line as well
+					XYDataset tlDataset = createBaselineDataset(dataFile, newDataFile, getPlotType());
+					ticPlot.addTICDataset(tlDataset);
+
+					this.crashed = false;
+				}
+			} catch (IOException e) {				// Writing error
+				e.printStackTrace();
+				this.crashed = true;
+				//baselineCorrector.initProgress(dataFile);
+			} catch (IllegalStateException e) {		// R computing error
+				e.printStackTrace();
+				this.crashed = true;
+				//baselineCorrector.initProgress(dataFile);
+			}
+
+			// Handle post-processing
+			if (!this.crashed) {
+				// If processing went fine: Restore "parametersChanged" listeners
+				unset_VK_ESCAPE_KeyListener();
+				// And remove temporary file
+				dataFile = null;
+			} else {
+				// If processing went wrong: Stop all & prepare for next attempt
+				baselineCorrector.setAbortProcessing(dataFile, true);
+				baselineCorrector.initProgress(dataFile);
+			}
+
+		}
+
+		public boolean getCrashed() {
+			return this.crashed;
+		}
+
+		public void kill() {
+			RawDataFile dataFile = getPreviewDataFile();
+			if (baselineCorrector != null && dataFile != null) { 
+				// Abort current process
+				baselineCorrector.setAbortProcessing(dataFile, true); 
+				unset_VK_ESCAPE_KeyListener();
+			}
+		}
+
+	}
+
+	class ProgressThread extends Thread {
+
+		private RawDataFile dataFile;
+
+		public ProgressThread(RawDataFile dataFile) {
+			this.dataFile = dataFile;
+		}
+
+		@Override
+		public void run() {
+
+			progressBar.setVisible(true);
+			int val = 0;
+			while (val < 100) 
+			{
+				if (previewThread != null && progressBar.isVisible()) {
+					int progressMax = baselineCorrector.getProgressMax(dataFile);
+					int progress = baselineCorrector.getProgress(dataFile);
+					val = (int) Math.round((progressMax == 0 ? 0.0 : 100.0 * (double) progress / (double) progressMax));
+					progressBar.setValue(val);
+				}
+			}
+			this.kill();
+		}
+
+		public void kill() {
+			// Kill preview processing
+			if (previewThread != null) { 
+				unset_VK_ESCAPE_KeyListener();
+				previewThread = null; 
+			}
+			progressBar.setVisible(false);
+		}
+	}
+
+
+	/**
+	 * This function add all the additional components for this dialog over the
+	 * original ParameterSetupDialogWithChromatogramPreview.
+	 */
+	@Override
+	protected void addDialogComponents() {
+
+		super.addDialogComponents();
+
+		progressBar = new JProgressBar();
+		progressBar.setValue(25);
+		progressBar.setStringPainted(true);
+		Border border = BorderFactory.createTitledBorder("Processing...     <Press \"ESC\" to cancel>    ");
+		progressBar.setBorder(border);
+		this.add(progressBar, BorderLayout.NORTH);
+		progressBar.setVisible(false);
 	}
 
 	/**
@@ -77,34 +287,18 @@ public class BaselineCorrectorSetupDialog extends ParameterSetupDialogWithChroma
 	@Override
 	protected void loadPreview(TICPlot ticPlot, RawDataFile dataFile, Range rtRange, Range mzRange) {
 
-		// First, remove all current data sets
-		ticPlot.removeAllTICDataSets();
-		ticPlot.setPlotType(this.getPlotType());
+		baselineCorrector.initProgress(dataFile);
 
-		// Add the original raw data file
-		int scanNumbers[] = dataFile.getScanNumbers(1, rtRange);
-		TICDataSet ticDataset = new TICDataSet(dataFile, scanNumbers, mzRange, null, this.getPlotType());
-		ticPlot.addTICDataset(ticDataset);
-
-		try {
-			// Create a new corrected raw data file
-			RawDataFile newDataFile = baselineCorrector.correctDatafile(dataFile, this.correctorParameters);
-
-			// If successful, add the new data file
-			if (newDataFile != null) {
-				int newScanNumbers[] = newDataFile.getScanNumbers(1, rtRange);
-				TICDataSet newDataset = new TICDataSet(newDataFile, newScanNumbers, mzRange, null, this.getPlotType());
-				ticPlot.addTICDataset(newDataset);
-
-				// Show the trend line as well
-				XYDataset tlDataset = createBaselineDataset(dataFile, newDataFile, this.getPlotType());
-				ticPlot.addTICDataset(tlDataset);				
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
+		// Run process
+		if (previewThread == null || previewThread.getCrashed()) {
+			previewThread = new PreviewThread(ticPlot, dataFile, rtRange, mzRange);
+			previewThread.start();
+			
+			// Run progress
+			progressThread = new ProgressThread(dataFile);
+			progressThread.start();
 		}
+
 	}
 
 	/**
@@ -122,7 +316,7 @@ public class BaselineCorrectorSetupDialog extends ParameterSetupDialogWithChroma
 		double intensity;
 		Scan sc, new_sc;
 		DataPoint dp, new_dp;
-		
+
 		// Get scan numbers from original file.
 		final int[] scanNumbers = dataFile.getScanNumbers(1);
 		final int numScans = scanNumbers.length;
